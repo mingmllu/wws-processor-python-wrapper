66f9cd547863c4c0e50d7a9683bdfd07d1056c80
Ming Lu <ming.ml.lu@nokia.com>
2018-10-11 12:34:30 -0400

    Add WWS processor for counting people

---
+++ wws_proc_count_persons.py
@@ -0,0 +1,30 @@
+from wws_wrap import StreamOperator, wws_start
+import json
+
+class PeopleCountingOperator(StreamOperator):
+
+    block_type = 'pywrap_count_people'
+    input_ports = [('in', str)]
+    output_ports = [('out', str)]
+
+    def __init__(self, inputs, outputs, **kwargs):
+        StreamOperator.__init__(self, inputs, outputs)
+
+    def process(self, data, key=None):
+        output_dict = { "count_person" : 0 }
+        # will set input type hints to JSON directly
+        data_double_quotes = data.replace("\'", "\"") #JSON strings must use double quotes
+        data_dict = json.loads(data_double_quotes)
+        if "payload" in data_dict:
+            if "objects" in data_dict["payload"]:
+                if "detections" in data_dict["payload"]["objects"]:
+                   detect_obj_list = data_dict["payload"]["objects"]["detections"]
+                   for detect_obj in detect_obj_list:
+                       if "class" in detect_obj:
+                           if detect_obj["class"]["name"] == "person":
+                             if detect_obj["confidence"] >= 0.7:
+                                 output_dict["count_person"] += 1
+        yield 'out', output_dict
+
+if __name__ == '__main__':
+    wws_start()
